54515c8da2381c55855a9f5caaa083d8bb308b80
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-11 17:06:21 +0530

    app

---
+++ notanet/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Nota from './components/Nota';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Nota />
+    </ChakraProvider>
+  );
+}
+<React/>
+export default App;
