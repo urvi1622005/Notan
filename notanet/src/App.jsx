f1361ef42f495ed8791990cc6be0120dccf6635a
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-15 22:05:34 +0530

    update

--- notanet/src/App.jsx
+++ notanet/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import Nota from './components/Nota';
+// import Nota from './components/Nota.jsx';
+import Cards from './components/Cards.jsx';
 
 function App() {
   return (
     <ChakraProvider>
-      <Nota />
+      {/* <Nota /> */}
+      <Cards />
     </ChakraProvider>
   );
 }
