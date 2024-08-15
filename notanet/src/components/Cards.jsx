bdf7402d43139fadc817bd4c20416def67563e92
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-15 22:05:50 +0530

    ..

---
+++ notanet/src/components/Cards.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Cards.css';
+import { FaHome} from 'react-icons/fa';
+
+const Cards = () => {
+  return (
+    <div className="cards-container">
+      <header>
+        <FaHome className="home-icon" />
+        <span>Home</span>
+      </header>
+      
+      <main>
+        <div className="card"></div>
+        <div className="card">
+        
+        </div>
+        <div className="card"></div>
+      </main>
+       </div>
+  );
+};
+
+export default Cards;
