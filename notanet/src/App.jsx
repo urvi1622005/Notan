8428810a90911a2c603f89df18760a5d20613c74
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-26 17:53:26 +0530

    app.jsx

--- notanet/src/App.jsx
+++ notanet/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import Nota from './components/Nota.jsx';
-import Cards from './components/Cards.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Nota.jsx';
+import contact from './components/Cards.jsx';
 
 function App() {
   return (
-    <ChakraProvider>
-      <Nota />
-      <Cards />
-    </ChakraProvider>
+    <Router>
+      <div>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<contact />} ></Route>
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
-<React/>
+
 export default App;
