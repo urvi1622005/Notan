dad692bf91edb4e08b2381045e4c3705d7203985
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-27 08:13:16 +0530

    main

--- notanet/src/main.jsx
+++ notanet/src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import Nota from './components/Nota.jsx';
-import Cards from './components/Cards.jsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>    <ChakraProvider>
-    
-      <Nota />
-      <Cards/>
-      
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
     </ChakraProvider>
-    </React.StrictMode>,
-)
+  </React.StrictMode>,
+);
