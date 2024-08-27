8f172e1432c9ced5c4dc10190aad3065d097fc64
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-08-28 00:32:56 +0530

    cards

--- notanet/src/components/Cards.jsx
+++ notanet/src/components/Cards.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import './Cards.css';
-import { FaHome} from 'react-icons/fa';
-
+import { FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Cards = () => {
-return (
-<div className="cards-container">
-<header>
-<FaHome className="home-icon" />
-<span>Home</span>
-</header>
+  return (
+    <div className="cards-container">
+      <header>
+        <FaHome className="home-icon" />
+        <span>Home</span>
+      </header>
 
+      {/* Replace Tab with a Link */}
+      <nav className="tab-navigation">
+        <Link to="/contact" className="tab-item">
+          Restaurant
+        </Link>
+        {/* You can add more tabs here */}
+      </nav>
 
-  <main>
-    <div className="card"></div>
-    <div className="card"></div>
-    <div className="card"></div>
-  </main>
-   </div>
-
-);
+      <main>
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
+        <div className="card">Card 3</div>
+      </main>
+    </div>
+  );
 };
-
 
 export default Cards;
