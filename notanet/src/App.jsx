2ee3fb12f49e1ae786633972de7a78c967f7103a
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-03 00:14:33 +0530

    app

--- notanet/src/App.jsx
+++ notanet/src/App.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Box, Flex, Button } from '@chakra-ui/react';
-import Home from './components/Nota';
-import Contact from './components/Cards';
+import { Box, Flex, } from '@chakra-ui/react';
+import Home from './components/Nota.jsx';
+import Contact from './components/Cards.jsx';
 
 function App() {
   return (
     <Router>
       <Box p={4}>
         <Flex mb={4} gap={2}>
-          <Button as={Link} to="/" colorScheme="teal">
+          {/* <Button as={Link} to="/" colorScheme="teal">
             Home
           </Button>
           <Button as={Link} to="/contact" colorScheme="teal">
             Contact 
-          </Button>
+          </Button> */}
         </Flex>
+        <Link>
         <Routes>
+          {/* <Link> */}
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          {/* </Link> */}
         </Routes>
+        </Link>
       </Box>
     </Router>
   );
