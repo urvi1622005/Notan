dc028bfe5511cd2a7340704ef8ee93b3177d3e84
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-22 17:49:31 +0530

    app.jsx

--- notanet/src/App.jsx
+++ notanet/src/App.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Box, Flex, } from '@chakra-ui/react';
-import Home from './components/Nota.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Box, Flex } from '@chakra-ui/react';
+import Nota from './components/Nota.jsx';
 import Roadmaps from './components/Cards.jsx';
+import Contact from'./components/Contact.jsx';
 
 function App() {
   return (
     <Router>
       <Box p={4}>
         <Flex mb={4} gap={2}>
-          {/* <Button as={Link} to="/" colorScheme="teal">
-            Home
-          </Button>
-          <Button as={Link} to="/contact" colorScheme="teal">
-            Contact 
-          </Button> */}
+          {/* Navigation buttons can be added here if needed */}
         </Flex>
-        <Link>
         <Routes>
-          {/* <Link> */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Nota />} />
           <Route path="/Roadmaps" element={<Roadmaps />} />
-          {/* </Link> */}
+          <Route path="/Contact" element={<Contact />} />
         </Routes>
-        </Link>
       </Box>
     </Router>
   );
 }
-<React/>
+
 export default App;
