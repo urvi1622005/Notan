b4ad93ff3a3bc3a82b2075c78a84ed04dfeb2cbc
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-23 07:25:37 +0530

    config

--- notanet/vite.config.js
+++ notanet/vite.config.js
@@ -1,9 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.pdf'],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-}
+})
+
